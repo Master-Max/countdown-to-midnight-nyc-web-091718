dad0c37e9548aefd6932d23782a5be7ebb9e430d
Max Petersen <mglxp5@gmail.com>
2018-09-02 19:51:15 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown(n)
-  x = 1
-  while x < n
-    puts "#{x} SECOND(S)!"
-    x += 1
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
   end
   "HAPPY NEW YEAR!"
 end
